6f529f9b54a973ee1915312b29d58f9bdd9fc6fb
Darshana Keerthirathna <keerthi.mac@gmail.com>
2024-08-24 09:01:09 +0530

    movie api app

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -3,18 +3,21 @@ class MovieModel {
   String overview;
   String backdropPath;
   String posterPath;
+  double voteAverage;
 
   MovieModel(
       {required this.title,
       required this.posterPath,
       required this.backdropPath,
-      required this.overview});
+      required this.overview,
+      required this.voteAverage});
 
   factory MovieModel.fromJson(Map<String, dynamic> json) {
     return MovieModel(
         title: json['title'],
         posterPath: json['poster_path'],
-        backdropPath: json['backdrop_path'],
-        overview: json['overview']);
+        backdropPath: 'https://image.tmdb.org/t/p/w500${json['backdrop_path']}',
+        overview: 'https://image.tmdb.org/t/p/w500${json['overview']}',
+        voteAverage: json['vote_average']);
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tmdb_api/models/movie_model.dart';
 import 'package:tmdb_api/services/api_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,16 +12,125 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: FilledButton(onPressed: (){
-          ApiService().getPopularMovies();
-        }, child: const Text("Get Popular Movies")),
-      ),
-    );
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade900,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Spacer(),
+                    const Text(
+                      "Movie Hub",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer()
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              FutureBuilder(
+                  future: ApiService().getPopularMovies(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CupertinoActivityIndicator();
+                    }
+                    List<MovieModel> movies = snapshot.data!;
+                    return CarouselSlider(
+                      options: CarouselOptions(
+                          height: size.height * 0.22, autoPlay: true),
+                      items: movies.map((movie) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: Container(
+                                width: size.width,
+                                decoration: BoxDecoration(
+                                    color: Colors.grey.shade900,
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image:
+                                            NetworkImage(movie.backdropPath))),
+                                child: Stack(
+                                  children: [
+                                    Positioned(
+                                        bottom: 3,
+                                        left: 3,
+                                        child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 8, vertical: 2),
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                color: Colors.black
+                                                    .withOpacity(0.5)),
+                                            child: Text(
+                                              movie.title,
+                                              style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold),
+                                            ))),
+                                    Positioned(
+                                        top: 3,
+                                        right: 3,
+                                        child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 8, vertical: 2),
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                color: Colors.black
+                                                    .withOpacity(0.4)),
+                                            child: Row(
+                                              children: [
+                                                Icon(
+                                                  Icons.star,
+                                                  color: Colors.yellow.shade600,
+                                                  size: 15,
+                                                ),
+                                                Text(
+                                                  movie.voteAverage.toString().substring(0,3),
+                                                  style: const TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                              ],
+                                            )))
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    );
+                  })
+            ],
+          ),
+        ));
   }
 }
