748ccb5e4df88dd201cd18bd188ef60a726ab068
Darshana Keerthirathna <keerthi.mac@gmail.com>
2024-08-30 09:03:14 +0530

    commit message

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -4,20 +4,27 @@ class MovieModel {
   String backdropPath;
   String posterPath;
   double voteAverage;
+  bool isAdult;
+  String releaseDate;
 
   MovieModel(
       {required this.title,
       required this.posterPath,
       required this.backdropPath,
       required this.overview,
-      required this.voteAverage});
+      required this.voteAverage,
+      required this.isAdult,
+      required this.releaseDate});
 
   factory MovieModel.fromJson(Map<String, dynamic> json) {
     return MovieModel(
-        title: json['title'],
-        posterPath: 'https://image.tmdb.org/t/p/w500${json['poster_path']}',
-        backdropPath: 'https://image.tmdb.org/t/p/w500${json['backdrop_path']}',
-        overview: json['overview'],
-        voteAverage: json['vote_average']);
+      title: json['title'],
+      posterPath: 'https://image.tmdb.org/t/p/w500${json['poster_path']}',
+      backdropPath: 'https://image.tmdb.org/t/p/w500${json['backdrop_path']}',
+      overview: json['overview'],
+      voteAverage: json['vote_average'],
+      isAdult: json['adult'],
+      releaseDate: json['release_date'],
+    );
   }
 }
--- lib/screens/movie_view/movie_view.dart
+++ lib/screens/movie_view/movie_view.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:tmdb_api/models/movie_model.dart';
 
 class MovieView extends StatefulWidget {
+  final MovieModel movie;
 
-final MovieModel movie;
-
-  const MovieView({super.key,required this.movie});
+  const MovieView({super.key, required this.movie});
 
   @override
   State<MovieView> createState() => _MovieViewState();
@@ -15,12 +14,45 @@ final MovieModel movie;
 class _MovieViewState extends State<MovieView> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(image: NetworkImage(widget.movie.backdropPath))
-        )
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              width: size.width,
+              height: size.height * 0.25,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    //colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.darken),
+                      image: NetworkImage(widget.movie.backdropPath),
+                      fit: BoxFit.cover)),
+              child: Stack(children: [
+                Positioned(
+                    top: 5,
+                    left: 5,
+                    child: BackButton(
+                      color: Colors.white,
+                      style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(
+                              Colors.black.withOpacity(0.5))),
+                    ))
+              ])),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.movie.title,
+                  style: const TextStyle(
+                      color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
